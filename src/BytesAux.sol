2ff4c64c5b8f6f9b0e59101a26719ec741045f95
NoFaceDev <NoFaceDev@protonmail.com>
2022-08-14 19:05:09 +0400

    Finished example

--- src/BytesAux.sol
+++ src/BytesAux.sol
@@ -1,37 +1,69 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.13;
+
+import "src/BytesLib.sol";
 
 /// @title Store uint as bytes and recover it
-contract BytesAux {
+contract BytesAux{
 
-  /// @dev I am using 20 to simulate what a typical aux bytes size looks like
-  bytes20 public aux;
+  // We use BytesLib since I am not comfortable dealing with bytes in assembly right now.
+  using BytesLib for bytes;
 
+  struct DemoStruct{
+    uint64 number;
+    bytes24 aux;
+  }
+
+  DemoStruct demo;
+  
   constructor() {}
   
-  /// @dev Helper function converts uint to bytes
-  function uintToBytes(uint128 value_) internal returns (bytes20 uintBytes){
-    uintBytes = new bytes(20);
-    assembly{ 
-      mstore(add(uintBytes,32),value_)
-    }
-  }
-
-  /// @dev Helper function converts bytes to uint
-  function bytesToUint(bytes20 bytes_) internal returns (uint128 value){
-    assembly { 
-      value := mload(add(aux, 20))
-    }
-  }
+  
 
   // Sets the aux bytes to a uint value
   function setAux(uint128 value_) external returns (bool status){
-    aux = uintToBytes(value_);
+    // Set the aux to a bytes24 version of the uint converted to bytes
+    demo.aux = bytes24(abi.encodePacked(value_));
+    status = true;
+  }
+
+  // Sets the aux bytes to two uint values of different sizes
+  function setAuxMultiple(uint128 firstValue_, uint64 secondValue_) external returns (bool status){
+    // Convert the uints to bytes
+    bytes memory firstValueBytes = abi.encodePacked(firstValue_);
+    bytes memory secondValueBytes = abi.encodePacked(secondValue_);
+    // Concatenate them using the concat function (it's about 100-150 gas more than BytesLib.concat)
+    bytes memory concatenatedBytes = bytes.concat(firstValueBytes,secondValueBytes);
+    // Set the aux to a bytes24 version of this object
+    demo.aux = bytes24(concatenatedBytes);
     status = true;
   }
 
   // Gets the uint value based on the aux bytes
-  function setAux(uint128 value_) external returns (uint128 value){
-    value = bytesToUint(aux);
+  function getAux(uint8 offset) external view returns (uint128 value){
+    // Copy the bytes from state to memory
+    bytes memory copy = abi.encodePacked(demo.aux);
+    // Use the bytesLib function to recover the value (with 0 offset)
+    value = BytesLib.toUint128(copy,offset);
   }
+
+  /// @dev Gets the uint value based on the aux bytes
+  function getAuxMultiple() external view returns (uint128 firstValue, uint64 secondValue){
+    // Copy the bytes from state to memory
+    bytes memory copy = abi.encodePacked(demo.aux);
+    // Use the bytesLib function to recover the value (with 0 offset)
+    firstValue = BytesLib.toUint128(copy,0);
+    // Use the bytesLib function to recover the value (with 16 byte offset)
+    secondValue = BytesLib.toUint64(copy,16); // 16 Bytes offset
+  }
+
+  // Gets the second uint value based on the aux bytes and an offset
+  function getAuxMultipleSecondValueOnly(uint8 offset) external view returns (uint64 value){
+    // Copy the bytes from state to memory
+    bytes memory copy = abi.encodePacked(demo.aux);
+    // Use the bytesLib function to recover the value (with 0 offset)
+    value = BytesLib.toUint64(copy,offset);
+  }
+
+  
 }
--- test/BytesAux.t.sol
+++ test/BytesAux.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
@@ -11,6 +11,52 @@ contract BytesAuxTest is Test {
 
     function setUp() external {
         bytesAux = new BytesAux();
+    }
+
+    // Check that we can use the bytes24 to store a uint128
+    function testWriteAndReadNoOffset(uint128 value_) public {
+        // Set the AUX
+        bool setStatus = bytesAux.setAux(value_);
+        if(setStatus!=true){
+            revert("Setter did not return true");
+        }
+        // Get the AUX
+        uint128 storedValue = bytesAux.getAux(0);
+        if(storedValue!=value_){
+            revert("Stored value is not equal to the initial value!");
+        }
+    }
+
+    // Check that we can use the bytes24 to store a uint128 (16 bytes) and a uint64 (8 bytes) separately
+    // We also retrieve them independently
+    function testWriteAndReadWithOffset(uint128 firstValue_, uint64 secondValue_) public {
+
+        // Set the AUX
+        bool setStatus = bytesAux.setAuxMultiple(firstValue_,secondValue_);
+        if(setStatus!=true){
+            revert("Setter did not return true");
+        }
+        // Get the AUX
+        uint128 storedFirstValue;
+        uint64 storedSecondValue;
+        (storedFirstValue, storedSecondValue) = bytesAux.getAuxMultiple();
+
+        console2.log("Stored first value is",storedFirstValue);
+        console2.log("Stored second value is",storedSecondValue);
+
+        // Check the values
+        if(storedFirstValue!=firstValue_){
+            revert("Stored value is not equal to the initial value!");
+        }
+        if(storedSecondValue!=secondValue_){
+            revert("Stored value is not equal to the initial value!");
+        }
+
+        // Try and get the second value *only* using our custom function
+        uint64 storedSecondValueAgain = bytesAux.getAuxMultipleSecondValueOnly(16); 
+        if(storedSecondValue!=storedSecondValueAgain){
+            revert("Stored value is not equal to the second stored value!");
+        }
     }
 
 }
